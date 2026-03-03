704f862743a7af78c41b17fa0bfe4d84190d9a81
Depressed Jellybean <depressed.jellybean1811@gmail.com>
2026-03-03 19:14:17 +0530

    STEP v3

---
+++ Exp1_BookMyStay.java
@@ -0,0 +1,9 @@
+public class UseCase1HotelBookingApp {
+public static void main(String[] args) {
+    System.out.println("=====================================");
+    System.out.println("Welcome to Book My Stay");
+    System.out.println("Hotel Booking Management System v1.0");
+    System.out.println("=====================================");
+}
+}
+
---
+++ Exp1_Palindrome.java
@@ -0,0 +1,11 @@
+public class Main {
+   static final String APP_NAME = "Palindrome Checker App";
+   static final String APP_VERSION = "Version 1.0";
+   public static void main(String[] args) {
+       System.out.println("=======================================");
+       System.out.println("        " + APP_NAME);
+       System.out.println("        " + APP_VERSION);
+       System.out.println("=======================================");
+       System.out.println("Application initialized successfully.");
+   }
+
